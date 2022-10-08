f213608a5e829c31f9db492c37475e3b1f9af1b4
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2022-10-08 20:53:20 -0300

    Correccion de PHP en Contacto

--- correo-contacto.php
+++ correo-contacto.php
@@ -1,5 +1,5 @@
 <?php
-    $to = "contacto@seguelpropiedades.cl";
+    $to = "correodepruebas2509@gmail.com";
     $subject = "Formulario de contacto";
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
